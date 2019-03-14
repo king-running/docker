4a9c5f595ae7a2f8036bc7715095c74754530907
lishulong <lishulong@wecash.net>
2019-03-14 11:22:17 +0800

    fix add tar dir

--- docker-flask-health/start.sh
+++ docker-flask-health/start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 pwd=`pwd`
+mkdir -p $pwd/tar
 pip3 install virtualenv
 virtualenv --no-site-packages venv
 source venv/bin/activate
 pip install -r requirement.txt
-path_name=${pwd}/app.tar.gz
-tar -zcf $path_name --exclude='*.gz' ./
+path_name=${pwd}/tar/app.tar.gz
+tar -zcf $path_name --exclude='./tar' ./
 echo 'finished!!'
