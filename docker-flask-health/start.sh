7ff9ba14c7f538783f9b6f34d72dcdc3d54d79ca
lishulong <lishulong@wecash.net>
2019-03-14 11:17:47 +0800

    fix exclude

--- docker-flask-health/start.sh
+++ docker-flask-health/start.sh
@@ -4,6 +4,6 @@ pip3 install virtualenv
 virtualenv --no-site-packages venv
 source venv/bin/activate
 pip install -r requirement.txt
-path_name=${pwd}/tar/app.tar.gz
-tar -zcf $path_name --exclude='./tar' ./
+path_name=${pwd}/app.tar.gz
+tar -zcf $path_name --exclude='*.gz' ./
 echo 'finished!!'
